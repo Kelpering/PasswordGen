0bed7db6f9ec54c74394ed17dc2729c5930c4611
kelpering <tristan8272@gmail.com>
2022-11-14 08:54:35 -0700

    Consts and vars set.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//Function descriptions
+    //Toggle(id of element) will hide element if shown, vice versa
+    //Show(id of element) only shows the element, no reverse
+    //Hide(id of element) only hides the element, no reverse
+
+//To Do
+    //On button click, reveal panel of options
+    //Submit button
+    //Hide panel
+    //Generate password off of those options
+    //Write to Text box
+    //Repeatable
+
+//Vars
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "[", "]", "{", "}", "+", ",", ".", "/", ":", ";", "<", ">", "=", "?", "\\", "`", "|", "~"];
+const matrix = [lowercase, uppercase, numbers, special]; //Choices will activate and deactivate which numbers can be used.
+var choices = []; //Add numbers of choices here. 0, 1, 2, 3 for lowercase, uppercase, numbers, special
+var length;
+
+//Lowercase - special are all checkmarks
+//Length is a dropdown box of 8-128 characters for a password
+//Functions
+
 function toggleHide(id)
 {
     var display = document.getElementById(id).style.display;
@@ -10,12 +36,6 @@ function toggleHide(id)
         hide(id);
     }
 }
-function show(id)
-{
-    // may need to specify how to display in certain cases.
-    document.getElementById(id).style.display = "block";
-}
-function hide(id)
-{
-    document.getElementById(id).style.display = "none";
-}
+
+function show(id) {document.getElementById(id).style.display = "block";}
+function hide(id) {document.getElementById(id).style.display = "none";}
