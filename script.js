f6cd5ea618c535945a323b02bb4a9e959551ddd5
kelpering <tristan8272@gmail.com>
2022-11-15 09:07:06 -0700

    a

--- script.js
+++ script.js
@@ -14,11 +14,15 @@
 //Vars
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "[", "]", "{", "}", "+", ",", ".", "/", ":", ";", "<", ">", "=", "?", "\\", "`", "|", "~"];
 const matrix = [lowercase, uppercase, numbers, special]; //Choices will activate and deactivate which numbers can be used.
 var choices = []; //Add numbers of choices here. 0, 1, 2, 3 for lowercase, uppercase, numbers, special
 var length;
+var output = "";
+var text = "Your New Password |" + output +"|"
+
+document.getElementById("output").value = text;
 
 //Lowercase - special are all checkmarks
 //Length is a dropdown box of 8-128 characters for a password
